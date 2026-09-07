d1ebaaff62191e6cf1b55b8af462ad4dfb32a5fa
sumit <smtchhajed@gmail.com>
2026-09-07 21:56:45 +0530

    15

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import { GlobalLayout } from './components/GlobalLayout';
 import Navbar from './components/Navbar';
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
 import { StrictMode } from 'react'
-import { hydrateRoot, createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
-
-// If react-snap has already pre-rendered the HTML, "hydrate" it.
-// Otherwise, render it normally (for local development).
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(
-    rootElement,
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-} else {
-  const root = createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
